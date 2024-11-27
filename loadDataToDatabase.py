884f48dc507e6d139f7ad1821a2143f0cfd626ae
Uditya Prakash <udityaprakash983@gmail.com>
2024-11-27 14:58:13 +0530

    added no sql option in project

--- loadDataToDatabase.py
+++ loadDataToDatabase.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from sqlalchemy import create_engine
 
+import pymongo
+
 def storeinsqlDB(addedfile, dburl, batchsize=100):
     try:
         conn = create_engine(dburl)
@@ -24,9 +26,31 @@ def storeinsqlDB(addedfile, dburl, batchsize=100):
         print("Batch "+ str(i//batchsize + 1) + " inserted into the database")
     print("all data inserted in database")
 
+def storeinnosql(file, mongoDburl, dbname, collectionname, batchsize=100):
+
+    client = pymongo.MongoClient(mongoDburl)
+    db = client[dbname]
+    collection = db[collectionname]
+
+    try:
+        data = pd.read_csv(file)
+    except Exception as e:
+        print("error in reading file ", e)    
+
+    datafiltering = data[(data['Device'] =='Smartphone') &(data['Age'] > 40)]
+    required_columns = datafiltering[['Device', 'Age', 'Country']]
+
+    for i in range(0, len(required_columns), batchsize):
+
+        batch = required_columns.iloc[i:i + batchsize]
+        collection.insert_many(batch.to_dict('records'))
+        print("Batch "+ str(i//batchsize + 1) + " inserted into the no sql database")
+    print("all data inserted in no sql db")
+
+
 inputfile = "NetflixUserbase.csv"
 
-choicedDB = input("Enter storage type (sql/nosql): ")
+choicedDB = input("Enter storage type (sql/nosql): ").strip().lower()
 
 if choicedDB == "sql":
     dburl = input("Enter SQL DB URL: ")
@@ -37,9 +61,7 @@ elif choicedDB == "nosql":
     mongoDburl = input("Enter Mongodb URL: ")
     dbname = input("Enter Mongodb Database name")
     collectionname = input("Enter collection name")
+
+    storeinnosql(inputfile, mongoDburl, dbname, collectionname)
 else:
     print("Invalid choice choosed")
-
-
-db_url = input("Enter SQL DB URL: ")
-storeinsqlDB(inputfile, db_url, 70)
